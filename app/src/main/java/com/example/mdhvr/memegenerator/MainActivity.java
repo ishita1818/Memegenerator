3f5301712c5e6549964ab4884374715805e8b7ff
Akshat Jain <akjn11@gmail.com>
2017-12-07 21:53:18 +0530

    "Choose Image" button added + Minor changes (#7)
    * Made the app scrollable
    Making the app scrollable solves issues like hiding of the "Generate" button and also makes the app usable in landscape orientation.
    
    * "Choose Image" button added + Minor changes
    Added a "Choose Image" button to allow user to pick any image from his local files.
    Also, some minor changes/improvements --
    --> Top and Bottom texts weren't retaining text on changing orientation.
    --> Some layout changes.

--- app/src/main/java/com/example/mdhvr/memegenerator/MainActivity.java
+++ app/src/main/java/com/example/mdhvr/memegenerator/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.mdhvr.memegenerator;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements topfragment.topfragmentlistener {
 
@@ -18,4 +29,50 @@ public class MainActivity extends AppCompatActivity implements topfragment.topfr
         bottomf.setMemetext(top, bottom);
 
     }
+
+
+    private int PICK_IMAGE_REQUEST = 1;
+    private static final String TAG = "MainActivity";
+
+
+    public void pickImage (View v){
+        Intent intent = new Intent();
+// Show only images, no videos or anything else
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+// Always show the chooser (if there are multiple options available)
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+            String[] projection = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+            cursor.moveToFirst();
+
+            Log.d(TAG, DatabaseUtils.dumpCursorToString(cursor));
+
+            int columnIndex = cursor.getColumnIndex(projection[0]);
+            String picturePath = cursor.getString(columnIndex); // returns null
+            cursor.close();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                ImageView imageView = (ImageView) findViewById(R.id.image);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
