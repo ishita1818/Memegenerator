c41131f4d3086efcbedbe17c3d8cbec285bc9fe1
ishita sharma <ishitasharma12699@gmail.com>
2017-12-13 23:21:09 +0530

    Major changes(retrieving bitmap of final image)

--- app/src/main/java/com/example/mdhvr/memegenerator/MainActivity.java
+++ app/src/main/java/com/example/mdhvr/memegenerator/MainActivity.java
@@ -1,35 +1,98 @@
 package com.example.mdhvr.memegenerator;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity implements topfragment.topfragmentlistener {
+public class MainActivity extends AppCompatActivity {
+
+    private static final int READ_STORAGE_PERMISSION_REQUEST_CODE = 100;
+    private Bitmap bitmap =null;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context= getApplicationContext();
+        Button choose_image_button= findViewById(R.id.choose_image_button);
+        Button preview_button= findViewById(R.id.preview_buttton);
+        final EditText top_text= findViewById(R.id.top_text);
+        final EditText bottom_text= findViewById(R.id.bottom_text);
+        final ImageView imageView= findViewById(R.id.preview_image);
+        choose_image_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickImage(view);
+            }
+        });
+        preview_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!checkPermissionForReadExtertalStorage()){
+                    try {
+                        requestPermissionForReadExtertalStorage();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                Bitmap bitmap1 = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+                Bitmap bitmap2 = bitmap1.copy(Bitmap.Config.ARGB_8888, true);
+                if(bitmap2!=null){
+                Log.e("value",bitmap2.isMutable()+" ");
+                Canvas canvas = new Canvas(bitmap2);
+                Paint paint= new Paint();
+                paint.setColor(Color.BLACK);
+                paint.setTextSize(70);
+                paint.setTextAlign(Paint.Align.CENTER);
+                int x= canvas.getWidth()/2;
+                canvas.drawText(top_text.getText().toString(),x,100,paint);
+                canvas.drawText(bottom_text.getText().toString(),x,canvas.getHeight()-30,paint);
+                imageView.setImageBitmap(bitmap2);
+                }
+                else
+                    Log.e("error","null");
+            }
+        });
     }
-
-    @Override
-    public void createMeme(String top, String bottom) {
-
-        bottomfragment bottomf = (bottomfragment) getSupportFragmentManager().findFragmentById(R.id.fragment2);
-        bottomf.setMemetext(top, bottom);
-
+    public boolean checkPermissionForReadExtertalStorage() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            int result = context.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
+            return result == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
     }
-
+    public void requestPermissionForReadExtertalStorage() throws Exception {
+        try {
+            ActivityCompat.requestPermissions((Activity) context, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    READ_STORAGE_PERMISSION_REQUEST_CODE);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
 
     private int PICK_IMAGE_REQUEST = 1;
     private static final String TAG = "MainActivity";
@@ -64,10 +127,10 @@ public class MainActivity extends AppCompatActivity implements topfragment.topfr
             cursor.close();
 
             try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
                 // Log.d(TAG, String.valueOf(bitmap));
 
-                ImageView imageView = (ImageView) findViewById(R.id.image);
+                ImageView imageView = (ImageView) findViewById(R.id.preview_image);
                 imageView.setImageBitmap(bitmap);
             } catch (IOException e) {
                 e.printStackTrace();
